11d2ff3c42d70361af805d8cb620db7762f47fa3
AlsammanAlsamman <smahmoud@ageri.sci.eg>
2024-03-29 23:10:29 +0200

    Delete unnecessary files and code

--- DNAOperations.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-############################################## About Author #########################################
-# Created by: Alsamman M. Alsamman                                                                  #
-# Emails: smahmoud [at] ageri.sci.eg or A.Alsamman [at] cgiar.org or SammanMohammed [at] gmail.com  #
-# License: MIT License - https://opensource.org/licenses/MIT                                        #
-# Disclaimer: The script comes with no warranty, use at your own risk                               #
-# This script is not intended for commercial use                                                    #
-#####################################################################################################
